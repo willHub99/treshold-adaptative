f05bdca5cba3212b2151f3b3aa5da9f7564193fb
Willian Rodrigo Huber <72114383+willHub99@users.noreply.github.com>
2021-11-24 18:37:29 -0300

    adicao de comentarios ao arquivo main

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import numpy as np
 import cv2
 
 
-INPUT_IMAGE =  'image/205.bmp'
+INPUT_IMAGE =  '205.bmp'
 
 #algoritmo para processar as imagens
 def process(img):
@@ -25,9 +25,8 @@ def process(img):
     
    #faz processo de abertura 
     img_erode = cv2.erode(thresh, kernel, iterations=2) #primeiro erode
-    img_dilated = cv2.dilate(img_erode, kernel,iterations=1) #primeiro Dilata
+    img_dilated = cv2.dilate(img_erode, kernel,iterations=1) #depois Dilata
     
-
     contagem(img_dilated,img) #chama função para contagem dos arrozes, passa a imagem sem ruídos e a original
     
     
@@ -40,7 +39,7 @@ def contagem(opening,img):
     
     area_m=[cv2.contourArea(contador) for contador in arroz]#área dos contornos fechados
     
-    mediana=statistics.median(area_m) #mediana
+    mediana=statistics.median(area_m) #mediana das áreas do contorno
     
     count = 0
     for i in range(len(arroz)): #for para os contornos do arroz
@@ -49,6 +48,7 @@ def contagem(opening,img):
             count += round(area/mediana) #faz a divisão da área pela mediana para estimar a quantidade de grãos
         else: #caso contrário
             count += 1 #soma mais um 
+            
     
     print ('Arrozes contados:',count) #printa o número de arrozes
     cv2.imshow("Detectados", img)#mostra a imagem com contornos 
@@ -58,7 +58,8 @@ def main ():
 
     # Abre a imagem 
     img = cv2.imread (INPUT_IMAGE, cv2.IMREAD_COLOR) 
-    #prepara caso dê erro na imagem
+    
+    #prepara caso dê erro ao abrir a imagem
     if img is None:
         print ('Erro abrindo a imagem.\n')
         sys.exit ()
